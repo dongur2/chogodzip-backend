33f13f4cbe6787b05b57bd463d58d3456335a3cd
dongur2 <holymolyguri@gmail.com>
2024-10-08 02:34:16 +0900

    Feat: 부동산 & 부동산과 대출 연결 & 고시원 테이블 SQL, 목 데이터
    - 대출 있는 데이터와 대출 없는 데이터 (고시원) 하나씩 추가

---
+++ DDL/RoomMock.sql
@@ -0,0 +1,75 @@
+INSERT INTO ROOM VALUES (default, 1, 37.4724714397, 126.9419475255, 'https://image.neoflat.net/zzjjt2-D6Vr5hjo5syswZbX68jQ=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/2502/1cda27c7-5bff-49dd-bc70-eea26037c851.JPG',0, default, default);
+INSERT INTO ROOM VALUES (default, 2, 37.5129688777494, 126.95079196306, 'https://image.neoflat.net/ZYIclVMY069hO2sAomrF-J_Osks=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/3032/c9733d1a-db62-4350-9e97-a10ea24f921b.webp',1, default, default);
+# INSERT INTO ROOM VALUES (default, 3, 37.585193299658876, 127.08818804904296, 'https://image.neoflat.net/ZYF5Vy57kM-0RRnqx2B-7DRwHBo=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/6804/c4e2dcfb-aa02-4223-a805-fdbb3c3f7e3a.jpg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 1, 37.5091221436056, 127.084896860756, 'https://image.neoflat.net/ZWIJS78ym78Ko7WVDzrZ-6ZcIC8=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/8674/e9016558-da9d-45e5-be4c-4225751b5d6f.jpeg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 2, 37.5244884281539, 126.864453127284, 'https://image.neoflat.net/zVelkHKU--4IzuFz9saWop5LYF0=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/2331/c9bb3ab9-beae-4bbc-9a01-e11e3e81e6a1.jpg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 3, 37.504251438723, 126.93620716769, 'https://image.neoflat.net/ZVc8ucwOUa5Ma43sFFstI0I46fc=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/10112/b0e981f3-855b-48f3-b6f7-ffe2bc4160d5.jpg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 4, 37.5035076222324, 127.113162749566, 'https://image.neoflat.net/zvBCpYUQ1qwS3dVR6lMIYjW8STM=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/6830/ac876def-2f2d-4793-a06d-bdf33fc25d42.jpg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 1, 37.5124933826657, 127.111506834731, 'https://image.neoflat.net/zUxQvk8vkGsKNEUvYKPKyBYR2Ps=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/8158/ce9cb0fc-ffc3-42e1-b948-4fc6f304814e.jpg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 2, 37.5562230745076, 126.935023901727, 'https://image.neoflat.net/ztyxzLltAzF9-o6D7YU5KGW2tP4=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/8688/20aa5736-53b6-4ee3-a62b-201abf7ad916.jpeg',0, default, default);
+# INSERT INTO ROOM VALUES (default, 3, 37.5110210434137, 127.018415946512, 'https://image.neoflat.net/ztxb4lhEBxKObwYcZlCJR-U783U=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/8735/06ad9acf-a8a1-4bd9-99da-311d57bcbf05.jpg',0, default, default);
+
+
+-- 대출 불가능 데이터
+INSERT INTO GOSIWON VALUES (default, 1, '보감고시텔', '08840', '서울특별시 관악구 서림 7길', '63', 30, 50, 100, 100, 10,
+                            'priToilet|priShower', 'creditCard|freeMeal', null, 'allowMoveIn',
+                            '보감고시텔은 💡 서울 도심에 위치한 아늑하고 깨끗한 고시원입니다! 🏢 지하철역과 버스정류장이 도보 5분 거리에 있어 🚆 교통이 편리하고, 주변에 편의점 🏪, 카페 ☕, 마트 🛒 등이 있어 생활하기 매우 편리합니다.
+
+🔑 안전하고 편리한 시설
+각 방은 프라이버시를 위해 개인 출입문에 비밀번호 도어락 🔒이 설치되어 있으며, 내부에는 개별 냉장고 ❄️, 에어컨 ❄️, 책상 📝, 옷장 등 기본 가구가 갖춰져 있습니다. 공용 공간에는 주방 🍳, 세탁실 🧺, 공용 욕실 🚿, 샤워실이 있어 쾌적하게 이용하실 수 있습니다.
+
+📶 무료 와이파이
+고시텔 전역에서 무료로 고속 와이파이를 이용할 수 있어 📱, 공부나 업무를 보기에 최적입니다.
+
+🧹 매일 청소 서비스
+공용 공간은 매일 청소 서비스 🧽로 관리되며, 항상 깨끗한 상태를 유지합니다.
+
+👥 남녀 층 분리
+남녀 층이 구분되어 있어 보다 안전하고 편안한 주거 환경을 제공합니다. 💁‍♀️💁‍♂️
+
+🏡 가격 정보
+
+월세: 30만 원부터 💸
+보증금: 100만 원부터 💰
+관리비: 5만 원 (전기, 수도, 인터넷 포함) 💡
+보감고시텔에서 🏠 편리하고 안전한 생활을 경험해보세요! 😊', 0, 0, 1, null, null
+                           , 'center', 'personal', 'bed|closet|desk|chair|airCon', 'cctv|digitLock'
+                           , 2, 0, 0, 0);
+
+-- 대출 가능 데이터
+INSERT INTO GOSIWON VALUES(default, 2
+                          ,'레몬트리 고시원', '06902', '서울 동작구 노량진로 223', '3층', 15, 30, 5, 10, 5
+                          ,'priToilet|priShower', 'securityCom|cleanCom|creditCard|freeMeal|manlessDeliveryBox'
+                          ,'eng', 'allowMoveIn|allowForeigner'
+                          ,'🌳 레몬트리 고시원에 오신 것을 환영합니다!
+
+레몬트리 고시원은 서울 동작구 노량진로 223에 위치한 고시원으로, 학생들과 직장인들에게 안락하고 편리한 생활을 제공합니다. 🏢 3층에 자리한 저희 고시원은 도심 속에서 조용한 환경을 제공하며, 다양한 편의시설과 함께 최적의 주거 조건을 자랑합니다.
+
+🏡 방 시설
+
+개인 화장실과 개인 샤워실이 각 방에 제공되어 프라이버시를 완벽하게 보호합니다.
+각 방에는 책상, 의자, 침대, 옷장 등 필요한 가구가 모두 갖춰져 있어 편리한 생활이 가능합니다. 🛏️🪑
+냉장고와 에어컨도 구비되어 있어, 사계절 내내 쾌적하게 지낼 수 있습니다. ❄️🌡️
+💡 다양한 편의시설
+
+공용 구역에서는 보안 시스템이 철저히 관리되고 있으며, 청소 서비스가 주기적으로 제공됩니다. 🧹✨
+무인 택배함을 이용할 수 있어 언제든 안전하게 물품을 수령할 수 있습니다. 📦
+카드 결제도 가능하며, 무료 식사 서비스도 제공되어 생활비 부담을 줄일 수 있습니다. 🍽️💳
+🌍 외국인도 환영!
+
+저희 고시원은 영어 서비스를 지원하여, 외국인 입주자들도 편하게 생활할 수 있습니다. 🌐
+외국인 입주 또한 가능하니, 다양한 국적의 분들도 환영합니다! 🗣️
+🔑 입주 조건
+
+월세: 15만 원부터 💸
+보증금: 5만 원부터 💰
+관리비: 5만 원 (전기, 수도, 인터넷 포함) 💡
+편안하고 안전한 생활을 원하신다면, 레몬트리 고시원에서 새로운 시작을 함께하세요! 😊'
+                            , 1, 0, 1, 15, 35
+                            , 'personal', 'personal'
+                            , 'bed|desk|chair|closet|refrig|dryer|hairDryer|tv|airCon'
+                            , 'cctv|fireAlarm|digitLock|fireKiller|springCooler'
+                            , 0, 0, 1, 0);
+
+-- 대출 연결
+INSERT INTO ROOM_LOAN VALUES(default, 2, 1);
