00b50435399ea5d005cc897a6e97973c2c9de4da
dongur2 <holymolyguri@gmail.com>
2024-10-10 01:11:14 +0900

    Test: 고시원 매물 작성 쿼리 테스트 추가
    - Room & Gosiwon INSERT

--- src/test/java/com/kb/room/mapper/RoomMapperTest.java
+++ src/test/java/com/kb/room/mapper/RoomMapperTest.java
@@ -7,6 +7,7 @@ import com.kb._config.WebMvcConfig;
 import com.kb.room.dto.request.BasicInfo;
 import com.kb.room.dto.request.GosiwonPostDTO;
 import com.kb.room.dto.request.RoomTempPostDTO;
+import com.kb.room.vo.Gosiwon;
 import com.kb.room.vo.RoomTemp;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
@@ -41,5 +42,53 @@ class RoomMapperTest {
 
         mapper.saveRoom(room);
         Assertions.assertEquals(14L, room.getRoomId());
+    }
+
+    @Test
+    @DisplayName("고시원 작성")
+    void saveGosiwon() {
+        RoomTemp room = RoomTemp.builder()
+                .userId(1L)
+                .roomLat(new BigDecimal("37.0996527684079"))
+                .roomLong(new BigDecimal("126.839982282494"))
+                .thumbnail("https://image.neoflat.net/vgTmOpt30n25GpYp_-4_SqXjBDE=/240x288/filters:no_upscale():watermark(/resource/gobang.png,center,center,0,20,none)/house/2760/abf4686a-658e-4324-82a0-ff62863003a6.jpg")
+                .canLoan(false)
+                .build();
+
+        mapper.saveRoom(room);
+
+        Gosiwon gosiwon = Gosiwon.builder()
+                .room(room)
+                .category("gosiwon")
+                .title("고시원 제목1")
+                .postcode("06903")
+                .address("서울특별시 관악구 서림 7길")
+                .detailAddress("99 4층")
+                .priceMin(40)
+                .priceMax(65)
+                .depositMin(10)
+                .depositMax(15)
+                .maintenanceFee(0)
+                .privateFacilities("개인화장실|개인샤워실")
+                .services("creditCard|freeMeal")
+                .languages("eng")
+                .desc("상세 설명....")
+                .genderLimit(0)
+                .type(0)
+                .contractMin(1)
+                .ageMin(0)
+                .ageMax(0)
+                .facilityHeating("center")
+                .facilityCooling("center")
+                .facilityLife("desk|chair")
+                .facilitySecurity("CCTV")
+                .buildingType(0)
+                .canParking(0)
+                .hasElevator(0)
+                .isSoldOut(false)
+                .build();
+
+        mapper.saveGosiwon(gosiwon);
+        Assertions.assertEquals(4L, gosiwon.getGswId());
     }
 }
