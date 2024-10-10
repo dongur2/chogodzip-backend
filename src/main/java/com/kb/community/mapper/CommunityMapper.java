364ade51a8dc54421013ad27f719aa36f7475a66
dongur2 <holymolyguri@gmail.com>
2024-10-10 18:41:02 +0900

    Feat & Test: 커뮤니티 전체 조회 쿼리 & 테스트 추가

--- src/main/java/com/kb/community/mapper/CommunityMapper.java
+++ src/main/java/com/kb/community/mapper/CommunityMapper.java
@@ -3,7 +3,10 @@ package com.kb.community.mapper;
 import com.kb.community.vo.Community;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface CommunityMapper {
+    List<Community> findAll();
     Community findById(@Param("communityId") Long communityId);
     Long save(Community community);
 }
--- src/test/java/com/kb/community/mapper/CommunityMapperTest.java
+++ src/test/java/com/kb/community/mapper/CommunityMapperTest.java
@@ -5,7 +5,6 @@ import com.kb._config.ServletConfig;
 import com.kb._config.WebConfig;
 import com.kb._config.WebMvcConfig;
 import com.kb.community.vo.Community;
-import com.kb.member.dto.Member;
 import com.kb.member.mapper.MemberMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -13,12 +12,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 
 @Slf4j
 @WebAppConfiguration
@@ -29,17 +28,24 @@ class CommunityMapperTest {
     @Autowired private MemberMapper memberMapper;
 
     @Test
-    @DisplayName("커뮤니티 글 작성")
+    @DisplayName("커뮤니티 글 작성 & ID로 단일 조회")
     void save() {
         Community vo = Community.builder()
                 .user(memberMapper.selectById("1L"))
-                .title("[테스트] 커뮤니티 제목 01")
+                .title("[테스트] 커뮤니티 제목 02")
                 .content("[테스트] 커뮤니티 컨텐츠 01")
                 .tag("부동산")
                 .build();
         mapper.save(vo);
 
         Community saved = mapper.findById(vo.getCommunityId());
-        Assertions.assertThat(saved.getTitle()).isEqualTo("[테스트] 커뮤니티 제목 01");
+        Assertions.assertThat(saved.getTitle()).isEqualTo("[테스트] 커뮤니티 제목 02");
+    }
+
+    @Test
+    @DisplayName("커뮤니티 글 전체 조회")
+    void findAll() {
+        List<Community> all = mapper.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(9);
     }
 }
