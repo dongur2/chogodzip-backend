0a00648e3f9ffe514e9cd4346dd2193be0227167
dongur2 <holymolyguri@gmail.com>
2024-10-10 18:17:11 +0900

    Test: 커뮤니티 글 작성 서비스 테스트 추가 [첨부파일 없음]

---
+++ src/test/java/com/kb/community/service/CommunityServiceTest.java
@@ -0,0 +1,39 @@
+package com.kb.community.service;
+
+import com.kb._config.RootConfig;
+import com.kb._config.ServletConfig;
+import com.kb._config.WebConfig;
+import com.kb._config.WebMvcConfig;
+import com.kb.community.dto.request.CommunityPostDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@Slf4j
+@WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {RootConfig.class, WebConfig.class, WebMvcConfig.class, ServletConfig.class})
+class CommunityServiceTest {
+    @Autowired private CommunityService service;
+
+    @Test
+    @DisplayName("커뮤니티 글 작성")
+    void add() {
+        CommunityPostDTO dto = CommunityPostDTO.builder()
+                .userId(3L)
+                .title("[서비스 테스트] 커뮤니티 제목")
+                .content("[서비스 테스트] 커뮤니티 본문")
+                .tag("realEstate")
+                .build();
+
+        Long added = service.add(dto);
+        Assertions.assertThat(added).isEqualTo(2L);
+    }
+}
