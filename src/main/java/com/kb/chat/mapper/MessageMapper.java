52d90f0783b3fba03919a44ae6b996f720716e74
mimmmji <taylor7418@gmail.com>
2024-10-12 17:53:23 +0900

    Feat: 채팅구현

--- src/main/java/com/kb/chat/mapper/MessageMapper.java
+++ src/main/java/com/kb/chat/mapper/MessageMapper.java
@@ -1,9 +1,15 @@
 package com.kb.chat.mapper;
 import com.kb.chat.dto.Message;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MessageMapper {
     List<Message> findMessagesByChatRoom(Long chatroomId);
     void insertMessage(Message message);
+    int countUnreadMessages(@Param("chatroomId") Long chatroomId, @Param("senderId") Long senderId);
+    void updateReadStatus(Long chatRoomId, Long senderId);
+
+    void updateReadStatus(Map<String, Object> params);
 }
