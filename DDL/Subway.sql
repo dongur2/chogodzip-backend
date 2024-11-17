cd726b3656ca1df2ed11a1101cf2bdafefbd42b2
dongur2 <holymolyguri@gmail.com>
2024-11-18 03:10:55 +0900

    Refactor(DB): 리뷰, 전철 관련 테이블 추가
    - 컬럼 일부 수정

---
+++ DDL/Subway.sql
@@ -0,0 +1,20 @@
+-- 전철 테이블
+drop table if exists SUBWAY;
+
+CREATE TABLE SUBWAY (
+    CODE	BIGINT PRIMARY KEY NOT NULL,    -- 전철역코드
+    NAME	VARCHAR(50)	    NOT NULL,       -- 전철역명
+    LINE	varchar(10)	    NOT NULL,       -- 호선
+    EX_CODE	VARCHAR(255)	NOT NULL        -- 외부코드
+);
+
+-- 매물과 가까운 전철역 테이블
+CREATE TABLE ROOM_SUBWAY (
+   RS_ID	    BIGINT PRIMARY KEY AUTO_INCREMENT,
+   CODE	        BIGINT	        NOT NULL,   -- 전철역코드
+   ROOM_ID	    BIGINT          NOT NULL,
+   DISTANCE	    BIGINT	        NOT NULL,   -- 전철역에서 집까지 거리
+   WALK	        VARCHAR(255)	NOT NULL,	-- 전철역에서 집까지 도보거리
+   CONSTRAINT FK_ROOM_SUBWAY_SUBWAY FOREIGN KEY (CODE) REFERENCES SUBWAY(CODE) ON DELETE CASCADE,
+   CONSTRAINT FK_ROOM_SUBWAY_ROOM FOREIGN KEY (ROOM_ID) REFERENCES ROOM(ROOM_ID) ON DELETE CASCADE
+);
